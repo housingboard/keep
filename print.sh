939474a018f6df10c0c2230813cb2fe4a37ddb16
housingboard <138868288+housingboard@users.noreply.github.com>
2026-01-29 13:01:14 +0530

    Add files via upload

---
+++ print.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+# File name
+PDF_FILE="print.pdf"
+
+# Get page count
+PAGE_COUNT=$(qpdf --show-npages "$PDF_FILE")
+
+echo "" | ps2pdf - empty.pdf
+
+# Check if even or odd
+if [ $((PAGE_COUNT % 2)) -eq 0 ]; then
+    echo "The PDF has an even number of pages: $PAGE_COUNT"
+    
+    PPDF_FILE="$PDF_FILE"
+    
+else
+    echo "The PDF has an odd number of pages: $PAGE_COUNT"
+    pdfunite "$PDF_FILE" empty.pdf print_d.pdf
+    PPDF_FILE=print_d.pdf
+fi
+
+
+echo Final_q_pdf $PPDF_FILE
+
+qpdf $PPDF_FILE --pages . 1-z:odd -- output_odd.pdf
+qpdf $PPDF_FILE --pages . z-1:odd -- output_even_reversed.pdf
+
+
+echo Final_q_pdfs generated
+
+
